c1a11b7daaf30d780230f3dbe37c3447d940125c
sworup <sworupkhadka1323@gmail.com>
2026-07-13 17:10:00 -0400

    add day 8

---
+++ src/ImmutableString.java
@@ -0,0 +1,8 @@
+public class  ImmutableString {
+    public static void main (String [] args){
+        String s= "Hello";
+        System.out.println(s.hashCode());
+        s="World";
+        System.out.println(s.hashCode());
+    }
+}
