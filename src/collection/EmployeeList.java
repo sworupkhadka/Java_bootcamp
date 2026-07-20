9b8bb159fc2aabd9f8d4d1b7f29e9e147ae9a387
sworup <sworupkhadka1323@gmail.com>
2026-07-20 15:37:02 -0400

    day 19

---
+++ src/collection/EmployeeList.java
@@ -0,0 +1,42 @@
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EmployeeList {
+
+    private String name;
+    private String address;
+    private double salary;
+    private int age;
+
+    // Parameterized constructor
+    public EmployeeList(String name, String address, double salary, int age) {
+        this.name = name;
+        this.address = address;
+        this.salary = salary;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                ", salary=" + salary +
+                ", age=" + age +
+                '}';
+    }
+
+    public static void main(String[] args) {
+
+        List<EmployeeList> employeeList = new ArrayList<>();
+
+        employeeList.add(new EmployeeList("Anish", "NJ", 10000.0, 28));
+
+
+        for (EmployeeList employee : employeeList) {
+            System.out.println(employee);
+        }
+    }
+}
---
+++ src/collection/SetDemo.java
@@ -0,0 +1,48 @@
+package collection;
+
+import java.util.*;
+
+public class SetDemo {
+    static void main(String[] args) {
+
+        //Set<Integer> numberList = new HashSet<>();                                          //hashset does not have insertion order  not maintained and duplicate values also not supported
+
+        // to make sure insert order is maintained just use LinkedHashSet
+        //TreeSet gives in ascending order 0-9, a-z (Set<Integer> numberList = new TreeSet<>(); for descending order give new TreeSet<>(Comparator.reverseOrder())
+
+        Set<Integer> numberList = new LinkedHashSet<>();
+
+        numberList.add(4);
+        numberList.add(5);
+        numberList.add(6);
+        numberList.add(7);
+        numberList.add(9);
+
+       // numberList.add(9);                                                                   //set does not add duplicate elements
+
+        //numberList.remove(9);                                                               //removes 9
+
+
+
+
+        System.out.println("--------------");
+        System.out.println(numberList.size());
+        System.out.println("--------------");
+
+
+        List<Integer> numToAddList= new ArrayList<>();
+        numToAddList.add(11);
+        numToAddList.add(12);
+        numToAddList.add(122);
+
+        numberList.addAll(numToAddList);
+
+
+        Iterator iterator = numberList.iterator();
+        while(iterator.hasNext()){
+            System.out.println(iterator.next());
+        }
+
+
+    }
+}
